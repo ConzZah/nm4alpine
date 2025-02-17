af3b63766f8d123e419d9e0dd1664dd1ea02f159
ConzZah <69615452+ConzZah@users.noreply.github.com>
2025-02-17 06:18:41 +0100

    Update nm4alpine.sh
    fix #1

--- nm4alpine.sh
+++ nm4alpine.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
   #===============================================
-  # Project: nm4alpine / v0.1
+  # Project: nm4alpine / v0.2
   # Author:  ConzZah / ©️ 2025
-  # Last Modification: 16.02.25 / 21:13
+  # Last Modification: 17.02.25 / 06:18
   # https://github.com/ConzZah/nm4alpine
   #===============================================
 
 nm="networkmanager"
 
 ## install packages:
-doas apk add "$nm" "$nm"-cli "$nm"-tui "$nm"-wifi "$nm"-wwan "$nm"-bluetooth "$nm"-bash-completion
+doas apk add "$nm" "$nm"-cli "$nm"-tui "$nm"-wifi "$nm"-wwan "$nm"-bluetooth "$nm"-bash-completion network-manager-applet
 
 # install NetworkManager.conf in:
 # /etc/NetworkManager/NetworkManager.conf
@@ -30,6 +30,10 @@ doas mv -f NetworkManager.conf /etc/NetworkManager/NetworkManager.conf
 
 ##########################################
 
+# add current user to plugdev group
+
+doas adduser "$USER" plugdev
+
 # stop conflicting services & add nm as default
 
 doas rc-service networking stop
@@ -38,4 +42,5 @@ doas rc-service "$nm" start
 doas rc-update add "$nm" default
 doas rc-update del networking boot
 doas rc-update del wpa_supplicant boot
+doas rc-service wpa_supplicant start
 echo -e "\nDONE, HAVE A GREAT DAY :D \n\nPRESS ANY KEY TO EXIT"; sleep 1; exit 
